cf5f2e27148cfd02a5054ff04b6bbc14c40ab1a1
Erick Lauretti <erick.nighyt@hotmail.com>
2023-01-11 17:00:08 -0300

    requisito 3 ok

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import FavoritePokemon from '../pages/FavoritePokemon';
+import renderWithRouter from '../renderWithRouter';
+
+describe('testando o componente FavoritePokemon', () => {
+  test('testando se exibe na tela a mensagem', () => {
+    renderWithRouter(<FavoritePokemon />);
+    expect(screen.getByText(/no favorite pokémon found/i)).toBeInTheDocument();
+  });
+});
