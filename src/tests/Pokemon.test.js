25ef1665be6d70d24b610e3d3f04919de81a5b33
Erick Lauretti <erick.nighyt@hotmail.com>
2023-01-11 17:58:13 -0300

    requisito 6 ok

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+import pokemonList from '../data';
+
+describe('testando o componente pokemon', () => {
+  test('testando se a imagem do pokemon esta correta', () => {
+    renderWithRouter(<App />);
+    const name = screen.getByTestId('pokemon-name').innerHTML;
+    const type = screen.getByTestId('pokemon-type');
+    const image = screen.getByRole('img');
+    const list = pokemonList.find((el) => el.name === name);
+    const bttn = screen.getByRole('link', {
+      name: /more details/i,
+    });
+    expect(image.src).toBe(list.image);
+    expect(image.alt).toBe(`${name} sprite`);
+    userEvent.click(bttn);
+    const favorit = screen.getByText(/pokémon favoritado\?/i);
+    userEvent.click(favorit);
+    const star = screen.getByRole('img', { name: `${name} is marked as favorite` });
+    expect(star.src).toBe('http://localhost/star-icon.svg');
+    expect(star.alt).toBe(`${name} is marked as favorite`);
+    expect(type).toHaveTextContent(list.type);
+    expect(bttn.href).toBe(`http://localhost/pokemon/${list.id}`);
+  });
+});
